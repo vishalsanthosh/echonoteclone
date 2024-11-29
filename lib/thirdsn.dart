8f0e5b42eafd4f92f0f26a5325db3dacc626a9f2
aleenakrish <aleenakrish59@gmail.com>
2024-11-29 13:28:24 +0530

    first commit

---
+++ lib/thirdsn.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatefulWidget {
+  const ThirdScreen({super.key});
+
+  @override
+  State<ThirdScreen> createState() => _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("howdy"),
+      ),
+    );
+  }
+}
